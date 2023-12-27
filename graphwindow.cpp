50807c39dc8f8ccb20e82c2984f0bd325e2ff922
mohamedAdhamc <mohamed.adhamnagiub@gmail.com>
2023-12-28 00:36:02 +0200

    add the ability to change width style and color

--- graphwindow.cpp
+++ graphwindow.cpp
@@ -1,5 +1,6 @@
 #include "graphwindow.h"
 #include "ui_graphwindow.h"
+#include <iostream>
 
 // Initialising the static variable
 int GraphWindow::FigureCounter=0;
@@ -9,6 +10,9 @@ GraphWindow::GraphWindow(DataSet *DataSet,QWidget *parent) :
     ui(new Ui::GraphWindow)
 {
     ui->setupUi(this);
+
+	// Install an event filter on CustomWidget
+	ui->customPlot->installEventFilter(this);
 
     // Increment the figure counter:
     FigureCounter++;
@@ -22,12 +26,71 @@ GraphWindow::GraphWindow(DataSet *DataSet,QWidget *parent) :
     // Setting the title of the figure
     this->setWindowTitle("Figure "+QString::number(FigureCounter));
 
+	// Constructing the context menu so it is ready to be called whenever
+	ConstructContextMenu(ContextMenu);
+
+	// connecting actions to responses via signal-slot mechanism:
+	{
+		//color change actions
+		connect(changeColorRed, &QAction::triggered, this, [this]() {
+			changeGraphColor(0, Qt::red);
+		});
+		connect(changeColorGreen, &QAction::triggered, this, [this]() {
+			changeGraphColor(0, Qt::green);
+		});
+		connect(changeColorBlack, &QAction::triggered, this, [this]() {
+			changeGraphColor(0, Qt::black);
+		});
+		connect(changeColorYellow, &QAction::triggered, this, [this]() {
+			changeGraphColor(0, Qt::yellow);
+		});
+	}
+
+	{
+		//width change actions
+		connect(changeWidth10, &QAction::triggered, this, [this]() {
+			changeGraphLineWidth(0, 1);
+		});
+		connect(changeWidth3, &QAction::triggered, this, [this]() {
+			changeGraphLineWidth(0, 3);
+		});
+		connect(changeWidth5, &QAction::triggered, this, [this]() {
+			changeGraphLineWidth(0, 5);
+		});
+		connect(changeWidth10, &QAction::triggered, this, [this]() {
+			changeGraphLineWidth(0, 10);
+		});
+
+	}
+
+	{
+		//line style change actions
+		connect(lineStyleNone, &QAction::triggered, this, [this]() {
+			changeGraphStyle(0, QCPGraph::lsNone);
+		});
+		connect(lineStyleLine, &QAction::triggered, this, [this]() {
+			changeGraphStyle(0, QCPGraph::lsLine);
+		});
+		connect(lineStyleStepLeft, &QAction::triggered, this, [this]() {
+			changeGraphStyle(0, QCPGraph::lsStepLeft);
+		});
+		connect(lineStyleStepCenter, &QAction::triggered, this, [this]() {
+			changeGraphStyle(0, QCPGraph::lsStepCenter);
+		});
+		connect(lineStyleImpulse, &QAction::triggered, this, [this]() {
+			changeGraphStyle(0, QCPGraph::lsImpulse);
+		});
+	}
 
 }
 
 GraphWindow::~GraphWindow()
 { // Called when the window of the figure is closed
     delete ui;
+	delete ContextMenu;
+	delete colorGrpah1;
+	delete widthGrpah1;
+	delete styleGrpah1;
 }
 
 void GraphWindow::SetGraphSetting(DataSet *DataSet)
@@ -35,11 +98,46 @@ void GraphWindow::SetGraphSetting(DataSet *DataSet)
 
     ui->customPlot->addGraph();
     ui->customPlot->graph(0)->addData(DataSet);
-    ui->customPlot->graph(0)->setPen(QPen(Qt::blue));
+	ui->customPlot->graph(0)->setPen(QPen(Qt::blue));
     ui->customPlot->graph(0)->setName(DataSet->getName());
     ui->customPlot->graph(0)->rescaleAxes();
-
 }
+
+void GraphWindow::contextMenuEvent(QContextMenuEvent *event)
+{
+	std::cout<<__PRETTY_FUNCTION__<<std::endl;
+// This function is called when the user right-clicks on the datasetwindow
+	ContextMenu->popup(event->globalPos()); // displaying the menu where the user clicks
+}
+
+void GraphWindow::ConstructContextMenu(QMenu *)
+{
+// This function is called in the constructor to build the context menu so that it does not need to be built everytime from scratch
+//	Graph1SubMenu->addAction(changeColor); // Add the action to the menu
+	colorGrpah1->addAction(changeColorRed);
+	colorGrpah1->addAction(changeColorBlack);
+	colorGrpah1->addAction(changeColorGreen);
+	colorGrpah1->addAction(changeColorYellow);
+
+	widthGrpah1->addAction(changeWidth1);
+	widthGrpah1->addAction(changeWidth3);
+	widthGrpah1->addAction(changeWidth5);
+	widthGrpah1->addAction(changeWidth10);
+
+	styleGrpah1->addAction(lineStyleNone);
+	styleGrpah1->addAction(lineStyleLine);
+	styleGrpah1->addAction(lineStyleStepLeft);
+	styleGrpah1->addAction(lineStyleStepCenter);
+	styleGrpah1->addAction(lineStyleImpulse);
+
+
+	Graph1SubMenu->addMenu(colorGrpah1);
+	Graph1SubMenu->addMenu(styleGrpah1);
+	Graph1SubMenu->addMenu(widthGrpah1);
+	ContextMenu->addMenu(Graph1SubMenu); // Add the submenus to the main menu
+}
+
+
 
 void GraphWindow::SetFigureSetting()
 { // Sets up the properties of the figure (that contains the curve)
@@ -55,4 +153,33 @@ void GraphWindow::SetFigureSetting()
     connect(ui->customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), ui->customPlot->xAxis2, SLOT(setRange(QCPRange)));
     connect(ui->customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), ui->customPlot->yAxis2, SLOT(setRange(QCPRange)));
     ui->customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+}
+
+void GraphWindow::changeGraphColor(int graphIndex, Qt::GlobalColor color){
+	QPen pen = ui->customPlot->graph(graphIndex)->pen();
+	pen.setColor(color);
+	ui->customPlot->graph(graphIndex)->setPen(pen);
+	ui->customPlot->replot();
+}
+
+void GraphWindow::changeGraphLineWidth(int graphIndex, int width)
+{
+	QPen pen = ui->customPlot->graph(graphIndex)->pen();
+	pen.setWidth(width);
+	ui->customPlot->graph(graphIndex)->setPen(pen);
+	ui->customPlot->replot();
+}
+
+void GraphWindow::changeGraphStyle(int graphIndex, QCPGraph::LineStyle lineStyle)
+{
+	ui->customPlot->graph(graphIndex)->setLineStyle(lineStyle);
+	ui->customPlot->replot();
+}
+
+bool GraphWindow::eventFilter(QObject *obj, QEvent *event)
+{
+	if (obj == ui->customPlot && event->type() == QEvent::ContextMenu) {
+		return true;
+	 }
+	return QDialog::eventFilter(obj, event);;
 }
--- graphwindow.h
+++ graphwindow.h
@@ -19,8 +19,9 @@
 
 #include <QDialog>
 #include "dataset.h"
-
-
+#include <QMenu>
+#include <vector>
+#include <qcustomplot.h>
 
 namespace Ui {
 class GraphWindow;
@@ -35,10 +36,47 @@ public:
     ~GraphWindow();
     void SetGraphSetting(DataSet *DataSet); // Function to control the setting of the curve (dataset represenation in the figure)
     void SetFigureSetting(); // Function to control the setting of the figure itself
+	void contextMenuEvent(QContextMenuEvent *event);
+	void ConstructContextMenu(QMenu *);
+
+	void changeGraphColor(int graphIndex, Qt::GlobalColor color);
+	void changeGraphLineWidth(int graphIndex, int width);
+	void changeGraphStyle(int graphIndex, QCPGraph::LineStyle lineStyle);
 
 private:
     Ui::GraphWindow *ui;
     static int FigureCounter; // Number of Figures created in the app ( defined as static because it is shared among all objects of this class)
+
+	// List of actions in the app (defined in the code rather than the UI)
+
+	//colors: red green yellow black
+	QAction* changeColorRed=new QAction("red", this);
+	QAction* changeColorGreen= new QAction("green", this);
+	QAction* changeColorYellow= new QAction("yellow", this);
+	QAction* changeColorBlack= new QAction("black", this);
+
+	//linewidth
+	QAction* changeWidth1=new QAction("1", this);
+	QAction* changeWidth3=new QAction("3", this);
+	QAction* changeWidth5=new QAction("5", this);
+	QAction* changeWidth10=new QAction("10", this);
+
+	//lineStyle
+	QAction* lineStyleNone=new QAction("none",this);
+	QAction* lineStyleLine=new QAction("line",this);
+	QAction* lineStyleStepLeft=new QAction("step left",this);
+	QAction* lineStyleStepCenter=new QAction("step center",this);
+	QAction* lineStyleImpulse=new QAction("impulse",this);
+
+	//Creating the menus (all accessable from the context menu):
+	QMenu *ContextMenu=new QMenu(this);
+	QMenu *Graph1SubMenu=new QMenu("Graph1"); // Plot section
+	QMenu *colorGrpah1=new QMenu("Color");
+	QMenu *styleGrpah1=new QMenu("Line Style");
+	QMenu *widthGrpah1=new QMenu("Line Width");
+
+protected:
+	bool eventFilter(QObject *obj, QEvent *event) override;
 };
 
 #endif // GRAPHWINDOW_H
